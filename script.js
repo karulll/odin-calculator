8e2866e2ae4519442345921bcfbf22c37dbbec09
karulll <180303552+karulll@users.noreply.github.com>
2025-11-03 23:07:58 +0800

    Add basic arithmetic functions and operator handling

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+function operate(numOne, numTwo, operator) {
+    switch(operator) {
+        case "+":
+
+            break;
+        case "-":
+            
+            break;
+        case "*":
+            
+            break;
+        case "/":
+            
+            break;
+        case "=":
+            
+            break;
+    }
+}
+
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+}
