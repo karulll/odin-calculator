85a13639c173cc90312ac7f026ad42ba7438d105
karulll <180303552+karulll@users.noreply.github.com>
2025-11-09 04:02:59 +0800

    Use prettier - code formatter - (I forgot to push to main befor switching linux and now some few hours of progress is gone unless i can somehow access unpushed commits?)

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const calcDisplay = document.querySelector('.result');
-const operationDisplay = document.querySelector('.result');
+const calcDisplay = document.querySelector(".result");
+const operationDisplay = document.querySelector(".result");
 
-const btnSound = document.querySelector('.btn-sound');
-const btnNegation = document.querySelector('.btn-negation');
-const btnAc = document.querySelector('.btn-ac');
-const btnC = document.querySelector('.btn-c');
-const btnDivide = document.querySelector('.btn-divide');
-const btnMultiply = document.querySelector('.btn-multiply');
-const btnSubtract = document.querySelector('.btn-subtract');
-const btnAdd = document.querySelector('.btn-add');
-const btnEquals = document.querySelector('.btn-equals');
-const btnDecimal = document.querySelector('.btn-decimal');
-const btn0 = document.querySelector('.btn-0');
-const btn1 = document.querySelector('.btn-1');
-const btn2 = document.querySelector('.btn-2');
-const btn3 = document.querySelector('.btn-3');
-const btn4 = document.querySelector('.btn-4');
-const btn5 = document.querySelector('.btn-5');
-const btn6 = document.querySelector('.btn-6');
-const btn7 = document.querySelector('.btn-7');
-const btn8 = document.querySelector('.btn-8');
-const btn9 = document.querySelector('.btn-9');
+const btnSound = document.querySelector(".btn-sound");
+const btnNegation = document.querySelector(".btn-negation");
+const btnAc = document.querySelector(".btn-ac");
+const btnC = document.querySelector(".btn-c");
+const btnDivide = document.querySelector(".btn-divide");
+const btnMultiply = document.querySelector(".btn-multiply");
+const btnSubtract = document.querySelector(".btn-subtract");
+const btnAdd = document.querySelector(".btn-add");
+const btnEquals = document.querySelector(".btn-equals");
+const btnDecimal = document.querySelector(".btn-decimal");
+const btn0 = document.querySelector(".btn-0");
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const btn3 = document.querySelector(".btn-3");
+const btn4 = document.querySelector(".btn-4");
+const btn5 = document.querySelector(".btn-5");
+const btn6 = document.querySelector(".btn-6");
+const btn7 = document.querySelector(".btn-7");
+const btn8 = document.querySelector(".btn-8");
+const btn9 = document.querySelector(".btn-9");
 
 addListener(btnSound);
 addListener(btnNegation);
@@ -44,49 +44,51 @@ addListener(btn8);
 addListener(btn9);
 
 function addListener(element) {
-    element.addEventListener("click", () => populateDisplay(element.dataset.value));
+  element.addEventListener("click", () =>
+    populateDisplay(element.dataset.value)
+  );
 }
-
+  
 let number = {
-    currentDisplay: ''
+  currentDisplay: "",
 };
 
 function populateDisplay(value) {
-    number.currentDisplay += value;
-    calcDisplay.textContent = number.currentDisplay;
+  number.currentDisplay += value;
+  calcDisplay.textContent = number.currentDisplay;
 
-    console.log(typeof number.currentDisplay, number.currentDisplay);
+  console.log(typeof number.currentDisplay, number.currentDisplay);
 }
 
 function operate(numOne, numTwo, operator) {
-    switch (operator) {
-        case "+":
-            add(numOne, numTwo);
-            break;
-        case "-":
-            subtract(numOne, numTwo);
-            break;
-        case "*":
-            multiply(numOne, numTwo);
-            break;
-        case "/":
-            divide(numOne, numTwo);
-            break;
-    }
+  switch (operator) {
+    case "+":
+      add(numOne, numTwo);
+      break;
+    case "-":
+      subtract(numOne, numTwo);
+      break;
+    case "*":
+      multiply(numOne, numTwo);
+      break;
+    case "/":
+      divide(numOne, numTwo);
+      break;
+  }
 }
 
 function add(numOne, numTwo) {
-    return numOne + numTwo;
+  return numOne + numTwo;
 }
 
 function subtract(numOne, numTwo) {
-    return numOne - numTwo;
+  return numOne - numTwo;
 }
 
 function multiply(numOne, numTwo) {
-    return numOne * numTwo;
+  return numOne * numTwo;
 }
 
 function divide(numOne, numTwo) {
-    return numOne / numTwo;
+  return numOne / numTwo;
 }
